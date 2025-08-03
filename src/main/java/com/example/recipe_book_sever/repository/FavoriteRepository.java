7b9d45ad5af427101f6bd0fc5b83700decb5ee91
이용현 <yh9035926@naver.com>
2025-08-03 19:34:19 +0900

    favoriteService

--- src/main/java/com/example/recipe_book_sever/repository/FavoriteRepository.java
+++ src/main/java/com/example/recipe_book_sever/repository/FavoriteRepository.java
@@ -5,8 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite,Long> {
+
+    boolean existsByUserIdAndRecipeId(String userId, Long recipeId);
     List<Favorite> findByUserId(String userId);
+
+    Optional<Favorite> findByUserIdAndRecipeId(String userId, Long recipeId);
+
 }
---
+++ src/main/java/com/example/recipe_book_sever/service/favoriteService.java
@@ -0,0 +1,53 @@
+package com.example.recipe_book_sever.service;
+
+import com.example.recipe_book_sever.model.Favorite;
+import com.example.recipe_book_sever.model.Recipe;
+import com.example.recipe_book_sever.model.User;
+import com.example.recipe_book_sever.repository.FavoriteRepository;
+import com.example.recipe_book_sever.repository.RecipeRepository;
+import com.example.recipe_book_sever.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class favoriteService {
+    private final FavoriteRepository favoriteRepository;
+    private final UserRepository userRepository;
+    private  final RecipeRepository recipeRepository;
+
+    @Transactional
+    public void addFavorite(String userId, Long recipeId){
+        User user= userRepository.findById(userId)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Recipe recipe=recipeRepository.findById(recipeId)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (favoriteRepository.existsByUserIdAndRecipeId(userId, recipeId)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 즐겨찾기한 레시피입니다.");
+        }
+
+        Favorite favorite=new Favorite();
+        favorite.setUser(user);
+        favorite.setRecipe(recipe);
+        favoriteRepository.save(favorite);
+    }
+
+    @Transactional
+    public void removeFavorite(String userId, Long recipeId) {
+             Favorite favorite = favoriteRepository.findByUserIdAndRecipeId(userId, recipeId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "즐겨찾기가 존재하지 않습니다."));
+
+        favoriteRepository.delete(favorite);
+    }
+
+    public List<Favorite> getUserFavorites(String userId) {
+        return favoriteRepository.findByUserId(userId);
+    }
+
+}
