eda33108b30b239fb60bbe9cae1ac6c7226ec085
이용현 <yh9035926@naver.com>
2025-08-13 18:59:08 +0900

    getCreatorRecipes

--- src/main/java/com/example/recipe_book_sever/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.recipe_book_sever.repository;
-
-import com.example.recipe_book_sever.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient,Long> {
-}
--- src/main/java/com/example/recipe_book_sever/repository/InstructionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.recipe_book_sever.repository;
-
-import com.example.recipe_book_sever.model.Instruction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructionRepository extends JpaRepository<Instruction,Long> {
-}
