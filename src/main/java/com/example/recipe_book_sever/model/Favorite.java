dc9086b0f61b1306e19529ed84c0f71ef838fb27
이용현 <yh9035926@naver.com>
2025-08-17 17:28:25 +0900

    favorite toggle

--- src/main/java/com/example/recipe_book_sever/model/Favorite.java
+++ src/main/java/com/example/recipe_book_sever/model/Favorite.java
@@ -1,7 +1,6 @@
 package com.example.recipe_book_sever.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
